9b170b45b25cabc316a545353773169235183b52
Martin Häggblom <martin.haggblom@gmail.com>
2023-12-14 09:59:04 +0200

    submit v7t3&4

--- viikko7_kivi-paperi-sakset/src/index.py
+++ viikko7_kivi-paperi-sakset/src/index.py
@@ -1,6 +1,4 @@
-from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
-from kps_tekoaly import KPSTekoaly
-from kps_parempi_tekoaly import KPSParempiTekoaly
+from luo_peli import luo_peli
 
 
 def main():
@@ -11,33 +9,17 @@ def main():
               "\n (c) Parannettua tekoälyä vastaan"
               "\nMuilla valinnoilla lopetetaan"
               )
+        
+        tyyppi = input()
+        peli = luo_peli(tyyppi)
 
-        vastaus = input()
-
-        if vastaus.endswith("a"):
+        if peli is not None:
             print(
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
-
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
+            peli.pelaa()
         else:
             break
-
 
 if __name__ == "__main__":
     main()
